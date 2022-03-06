3f2a3f370029ce9e68ee79c7a08bb6caef35e024
Allan <matrixboston7613@gmail.com>
2022-03-06 23:12:58 +0800

    number guesser complete

--- NumberGuess/NumberGuess.java
+++ NumberGuess/NumberGuess.java
@@ -1,37 +1,7 @@
-import java.util.Scanner; 
-import java.util.Random;
+import packages.MainPackageClass;
+
 public class NumberGuess {
-
-    public static final int RANDOM_NUMBER = new Random().nextInt(100);
-
-    /* Function to read user string input from console */
-    public static String readString(String question){
-        Scanner input = new Scanner(System.in);
-        System.out.print(question);
-        String response = input.next();
-        return response;
-    }
-    
-    /* Method to get name from user*/
-    public static String getName(){
-        String name = readString("Hello, what's your name? ");
-        return name;   
-    }
-
-    /* Method to ask user if they would like to play game*/
-    public static boolean willPlay(){
-        System.out.print("Hi " + getName());
-        String answer = readString(" Would you like to play guess the number? (y/n) ").toLowerCase();
-        while (!answer.equals("y") && !answer.equals("n")) {
-            answer = readString("Please enter either y or n ").toLowerCase();
-        }
-        System.out.println(answer.equals("y"));
-        return answer.equals("y");   
-    }
-
-    
-
     public static void main(String[] args) {
-        willPlay();
+        MainPackageClass.program();
     }
 }
---
+++ NumberGuess/packages/MainPackageClass.java
@@ -0,0 +1,57 @@
+package packages;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class MainPackageClass {
+    public static Scanner input = new Scanner(System.in);
+
+    /* Function to read user string input from console */
+    public static String readString(String question) { 
+        System.out.print(question);
+        String response = input.next();
+        return response;
+    }
+
+
+    /* Method to get name from user */
+    public static String getName() {
+        String name = readString("Hello, what's your name? ");
+        return name;
+    }
+
+    /* Method to ask user if they would like to play game */
+    public static boolean willPlay(String name, String question) {
+        System.out.println("Yo " + name);
+        String answer = readString(question + " ? (y/n) ").toLowerCase();
+        while (!answer.equals("y") && !answer.equals("n")) {
+            answer = readString("Please enter either y or n ").toLowerCase();
+        }
+        return answer.equals("y");
+    }
+
+    /* The main number guessing game */
+    public static void game(int randomNumber) {
+        int guess = (int) Math.round(Double.parseDouble(readString("Guess a number between 0 and 100 ")));
+        while (guess != randomNumber) {
+            if (guess > randomNumber)
+                System.out.println("Hmm, the number I'm thinking of is lower than " + guess);
+            else {
+                System.out.println("Hmm, the number I'm thinking of is higher than " + guess);
+            }
+            guess = (int) Math.round(Double.parseDouble(readString("Try guessing again! ")));
+        }
+        System.out.println("Nice, the number I was thinking of is " + randomNumber);
+    }
+
+    /* The main program */
+    public static void program() {
+        String name = getName();
+        boolean isPlaying = willPlay(name, "Would you like to play Number Guesser");
+        while (isPlaying) {
+            game(new Random().nextInt(100));
+            isPlaying = willPlay(name, "Would you like to play Number Guesser again");
+        }
+        System.out.println("Thanks for stopping by!");
+    }
+}
